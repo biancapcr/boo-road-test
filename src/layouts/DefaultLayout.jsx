837075af996080b0fe97c5f9a302792a02fbe351
biancapcr <biancacristina.pocora@gmail.com>
2025-09-27 17:18:30 +0200

    [fix] add global login and register modals to defaultlayout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Login from "../components/Login";
+import Registrati from "../components/Registrati";
 
-function DefaultLayout({ children, onLoginClick, onRegisterClick }) {
+function DefaultLayout({ children }) {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
   return (
     <div className="default-layout">
-      <Header onLoginClick={onLoginClick} onRegisterClick={onRegisterClick} />
+      <Header
+        onLoginClick={() => setShowLogin(true)}
+        onRegisterClick={() => setShowRegister(true)}
+      />
+
       <main>{children}</main>
+
+      {/* modali globali */}
+      <Modal
+        show={showLogin}
+        onClose={() => setShowLogin(false)}
+        title="Accedi"
+      >
+        <Login />
+      </Modal>
+
+      <Modal
+        show={showRegister}
+        onClose={() => setShowRegister(false)}
+        title="Registrati"
+      >
+        <Registrati />
+      </Modal>
     </div>
   );
 }
