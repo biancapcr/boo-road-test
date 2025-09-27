5adbe1ea13b8155284dd147049d76bf5020077f8
SergioGBosco <sergiogibosco@gmail.com>
2025-09-27 18:39:36 +0200

    Aggiunta_pagina di contatti  e dettaglio prodotti

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Rubrica from "./pages/Rubrica";
+import ContactPage from "./pages/ContactPage";
+import DefaultLayout from "./layouts/DefaultLayout";
+import DetailTripsPage from "./pages/DetailTripsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/viaggi" element={<DetailTripsPage />} />
         <Route path="/rubrica" element={<Rubrica />} />
+        <Route path='/contatti' element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   );
